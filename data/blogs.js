a3984424ed080313dbae84ef253b5cf74a37c345
annusinghraj1998@gmail.com <annusinghraj1998@gmail.com>
2022-02-14 17:59:43 +0530

    Fisrt commit

---
+++ data/blogs.js
@@ -0,0 +1,25 @@
+blogs = [
+    {
+        title: "How to get started with Python",
+        content: "This is content Python",
+        slug: "Python-learn"
+    },
+    {
+        title: "How to get started with Js",
+        content: "This is content Js",
+        slug: "Js-learn"
+    },
+    {
+        title: "How to get started with Django",
+        content: "This is content Django",
+        slug: "Django-learn"
+    },
+    {
+        title: "How to get started with CSS",
+        content: "This is content CSS",
+        slug: "CSS-learn"
+    },
+]
+
+
+module.exports = blogs;
